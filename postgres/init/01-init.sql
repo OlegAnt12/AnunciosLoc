8452386259d44cb23f2016bae4e0ff8ab0f1da47
Oleg <50102101+OlegAnt12@users.noreply.github.com>
2025-10-22 14:46:33 +0000

    configuração do ambiente

--- postgres/init/01-init.sql
+++ postgres/init/01-init.sql
@@ -1,5 +1,5 @@
 -- executado apenas na primeira inicialização do volume
-CREATE USER bits WITH PASSWORD 'bits123';
-CREATE DATABASE anunciosloc_bd OWNER bits;
-GRANT ALL PRIVILEGES ON DATABASE anunciosloc_bd TO bits;
+CREATE USER grupo15 WITH PASSWORD 'grupo15';
+CREATE DATABASE anunciosloc_bd OWNER grupo15;
+GRANT ALL PRIVILEGES ON DATABASE anunciosloc_bd TO grupo15;
 -- podes acrescentar schema e dados seed aqui (02-schema.sql, 03-seed.sql, ...)
